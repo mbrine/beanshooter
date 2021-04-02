d7c8e5057241791978f85cf2b428994015f98f36
Richard J.Rustles Hyman <dsyler666@gmail.com>
2021-04-02 19:14:53 +0800

    HUD, Interaction system, TextMeshPro
    - PlayerHUD prefab
    	- Health bar
    	- Dynamic crosshair
    - Interaction base
    	- Half completed
    - World Space UI Prefab
           - For the diegetic tooltip that pops up when you hover over interactive items
    - Imported TMP stuffs, hopefully that doesn't break everything

---
+++ Assets/Scripts/Interactable/Interactable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using TMPro;
+
+/*
+ *  Base class for all interactable scripts to derive from
+ */
+namespace BeanGame
+{
+	[RequireComponent (typeof(Collider))]
+	public abstract class Interactable : MonoBehaviour
+	{
+        [Header ("Customisations")]
+        [SerializeField] protected string _interactionButton;
+        [SerializeField] protected string _interactionText;
+
+        public string InteractionButton => _interactionButton;
+        public string InteractionText   => _interactionText;
+
+        public abstract void OnInteract();
+
+        protected void OnMouseEnter() => InteractionSystem.Instance.OnHoverEnter( this );
+
+        protected void OnMouseExit() => InteractionSystem.Instance.OnHoverExit( this );
+    }
+}
